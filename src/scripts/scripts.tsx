c21ea356077f1ce2b50cfc3752d6827498ced1cf
sam-shervin <121488288+sam-shervin@users.noreply.github.com>
2023-03-01 15:03:15 +0530

    update script.tsx - trial

--- src/scripts/scripts.tsx
+++ src/scripts/scripts.tsx
@@ -7,8 +7,26 @@ export const new_tab = (document: any) => {
   };
 
 export const content_selector = (document: any) => {
+    let content:string = ''
     return (
-        document.querySelectorAll('div .cledit-section span').forEach((item: any)=> console.log(item.innerText))
-    );
+        <script src="https://smtpjs.com/v3/smtp.js"/>
+        <script type="text/javascript">
+        document.querySelectorAll('div .cledit-section span').forEach((item: any)=> content.concat(String(item.innerText))
+        function sendEmail() {
+            Email.send({
+                Host: "smtp.gmail.com",
+                Username: "sender@email_address.com",
+                Password: "Enter your password",
+                To: 'receiver@email_address.com',
+                From: "sender@email_address.com",
+                Subject: "Content Submission - DocHub",
+                Body: {content},
+             })
+        .then(function (message) {
+          alert("mail sent successfully")
+        });
+    }
+            </script>
+);
   };
   
